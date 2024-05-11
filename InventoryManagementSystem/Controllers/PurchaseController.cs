4a82b44c48024a7105f20387aa16ab88efa017d9
omar faruk <faruk141pau@gmail.com>
2024-05-11 12:11:35 +0600

    Add project files.

---
+++ InventoryManagementSystem/Controllers/PurchaseController.cs
@@ -0,0 +1,111 @@
+﻿using InventoryManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InventoryManagementSystem.Controllers
+{
+    public class PurchaseController : Controller
+    {
+
+        Inventory_managementEntities db = new Inventory_managementEntities();
+        // GET: Purchase
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult DisplayPurchase()
+        {
+            List<Purchase> list =db.Purchases.OrderByDescending(x => x.id).ToList();    
+            return View(list);
+        }
+
+        [HttpGet]
+        public ActionResult PurchaseProduct()
+        {
+            List<string> list = db.Products.Select(x => x.Product_name).ToList();
+            ViewBag.ProductName=new SelectList(list);
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult PurchaseProduct(Purchase pur)
+        {
+            db.Purchases.Add(pur);
+            db.SaveChanges();
+            return RedirectToAction("DisplayPurchase");
+        }
+
+        //details
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            Purchase p= db.Purchases.Where(x =>x.id==id).SingleOrDefault();
+            return View(p); 
+               
+        }
+
+
+
+        //GET DELETE
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            Purchase p = db.Purchases.Where(x => x.id == id).SingleOrDefault();
+            return View(p);
+
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id, Purchase per)
+        {
+            Purchase p = db.Purchases.Where(x => x.id == id).SingleOrDefault();
+            db.Purchases.Remove(p);
+            db.SaveChanges();
+            return RedirectToAction("DisplayPurchase");
+        }
+
+
+        //GET EDIT
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            Purchase p = db.Purchases.Where(x => x.id == id).SingleOrDefault();
+            List<string> list = db.Products.Select(x => x.Product_name).ToList();
+            ViewBag.ProductName = new SelectList(list);
+            return View(p);
+        }
+        [HttpPost]
+        public ActionResult Edit(int id,Purchase pur)
+        {
+            Purchase p = db.Purchases.Where(x => x.id == id).SingleOrDefault();
+            p.Purchase_date = pur.Purchase_date;
+            p.Purchase_prod=pur.Purchase_prod;
+            p.Purchase_qunty = pur.Purchase_qunty;
+            db.SaveChanges();
+            return RedirectToAction("DisplayPurchase");
+        }
+        ////GET EDIT
+        //[HttpGet]
+        //public ActionResult Edit(int id)
+        //{
+        //    Purchase p = db.Purchases.Where(x => x.Id == id).SingleOrDefault();
+        //    List<string> list = db.Products.Select(x => x.Product_name).ToList();
+        //    ViewBag.ProductName = new SelectList(list);
+        //    return View(p);
+        //}
+        //[HttpPost]
+        //public ActionResult Edit(int id, Purchase pur)
+        //{
+        //    Purchase p = db.Purchases.Where(x => x.Id == id).SingleOrDefault();
+        //    p.Purchase_date = pur.Purchase_date;
+        //    p.Purchase_prod = pur.Purchase_prod;
+        //    p.Purchase_qnty = pur.Purchase_qnty;
+        //    db.SaveChanges();
+        //    return RedirectToAction("DisplayPurchase");
+        //}
+    }
+}
